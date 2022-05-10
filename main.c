96e778e7c5aa34f98d07f9b73a830adb463ba7a5
oxyvans <benja.1999.g@hotmail.com>
2022-05-10 11:37:51 -0700

    main

--- main.c
+++ main.c
@@ -23,8 +23,24 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-//-----------------------reading--------------------------
-	
+
+	R_File(fp);
+
 	fclose(fp);
 	return(0);
+}
+
+/**
+ * R_File - reding file
+ * @fp: file
+ */
+
+void R_File(FILE *fp)
+{
+	int linenum;
+	char *p, buf[1024];
+
+	for (linenum = 1; fgets(buf, sizeof(buf), fp) != NULL; ++linenum)
+	{
+	}
 }
