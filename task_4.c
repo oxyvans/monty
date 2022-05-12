5a449f853a6da4b53e4fbebc548bde78fff6ee06
oxyvans <benja.1999.g@hotmail.com>
2022-05-12 06:55:08 -0700

    fix error

--- task_4.c
+++ task_4.c
@@ -12,7 +12,7 @@ void _add(stack_t **stack, unsigned int line_number)
 
 	if (aux == NULL || aux->prev == NULL)
 	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
