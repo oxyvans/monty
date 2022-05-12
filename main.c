1aa165e3d0f6b1d3e32814f9f4df5f9d5d82edf2
oxyvans <benja.1999.g@hotmail.com>
2022-05-12 06:46:30 -0700

    task 5

--- main.c
+++ main.c
@@ -75,6 +75,7 @@ int search(char *buf, unsigned int linenum, stack_t **stack)
 		{"pop", _pop},
 		{"swap", _swap},
 		{"add", _add},
+		{"nop", _nop},
 		{NULL, NULL}
 	};
 
---
+++ task_5.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - nop
+ * @stack: stack
+ * @line_number: line
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
